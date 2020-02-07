3a336ec5111d8a155c217c6775867d0b97eca573
amit-kumar-mahato <amitraj0589@gmail.com>
2020-02-07 19:36:39 +0530

    [Amit] update NoteList.jsx

--- my-app/src/Components/NoteList.jsx
+++ my-app/src/Components/NoteList.jsx
@@ -1,142 +1,185 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 import Note from "../Components/Note";
 import Reminder from "../Components/Reminder";
 import SideDrawer from "../Components/SideDrawer";
-class NoteList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pinnedNote: [],
-      unPinnedNote: [],
-      archiveNote: [],
-      titles: [],
-      trashNote: []
-    };
-  }
-  test=()=>{
-    console.log("Archive :",this.state.archiveNote);
-    console.log("Trash :",this.state.trashNote);
-
-  }
-  static getDerivedStateFromProps(props, state) {
-    return {
-      pinnedNote: props.titles.filter(note => {
+import Axios from "axios";
+import NoteController from "../Controller/NoteController";
+function NoteList(props) {
+  const [notelist, setNoteList] = useState([]);
+  const [trashNote, setTrashNote] = useState([]);
+  const [pinnedNote, setPinnedNote] = useState([]);
+  const [unPinnedNote, setUnPinnedNote] = useState([]);
+  const [archiveNote, setArchiveNote] = useState([]);
+  useEffect(() => {
+    NoteController.allNotes().then(response => {
+      setNoteList(response.data.obj);
+    });
+  }, []);
+  useEffect(() => {
+    console.log("noteList", notelist);
+    setPinnedNote(
+      notelist.filter(note => {
         return note.pin;
-      }),
-      unPinnedNote: props.titles.filter(note => {
-        return !note.pin;
-      }),
-      archiveNote: props.titles.filter(note => {
+      })
+    );
+    setUnPinnedNote(
+      notelist.filter(note => {
+        return !note.pin && !note.archiev && !note.trash;
+      })
+    );
+    setArchiveNote(
+      notelist.filter(note => {
         return note.archiev;
-      }),
-      trashNote: props.titles.filter(note => {
+      })
+    );
+    setTrashNote(
+      notelist.filter(note => {
         return note.trash;
       })
-    };
-  }
-
-  updateNoteList(id) {
-    this.setState({
-      titles: this.titles.map(note => {
-        console.log(note.noteId);
-        if (note.noteId === id) {
-          note.pin = !note.pin;
-        }
-      })
-    });
-  }
-
-  updateTitle(id) {
-    this.setState({
-      titles: this.titles.map(note => {
-        console.log(note.noteId);
-        if (note.noteId === id) {
-          note.title = document.getElementById(id + "title").value;
-        }
-      })
-    });
-  }
-  render() {
-    this.titles = this.props.titles;
-    return (
-      <div>
-        {this.props.status.active ? (
-          <div>
-            <strong style={{ paddingLeft: "18px" }}>PINNED</strong>
-            <div className="note-list row">
-              {this.state.pinnedNote.map(note => {
-                return (
-                  <Note
-                    noteId={note.noteId}
-                    title={note.title}
-                    description={note.description}
-                    pin={note.pin}
-                    fnote={note}
-                    onPinClick={() => this.updateNoteList(note.noteId)}
-                  />
-                );
-              })}
-            </div>
-            <strong style={{ paddingLeft: "18px" }}>OTHERS</strong>
-            <div className="note-list row">
-              {this.state.unPinnedNote.map(note => {
-                return (
-                  <Note
-                    noteId={note.noteId}
-                    title={note.title}
-                    description={note.description}
-                    pin={note.pin}
-                    fnote={note}
-                    onPinClick={() => this.updateNoteList(note.noteId)}
-                    onArchiveClick={() => this.updateNoteList(note.noteId)}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        ) : this.props.status.archive ? (
-          <div>
-            <div className="note-list row">
-              {this.state.archiveNote.map(note => {
-                return (
-                  <Note
-                    noteId={note.noteId}
-                    title={note.title}
-                    description={note.description}
-                    pin={note.pin}
-                    fnote={note}
-                    onPinClick={() => this.updateNoteList(note.noteId)}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        ) : (
-          <div>
-            <div className="note-list row">
-              {this.state.trashNote.map(note => {
-                return (
-                  <Note
-                    noteId={note.noteId}
-                    title={note.title}
-                    description={note.description}
-                    pin={note.pin}
-                    fnote={note}
-                    onPinClick={() => this.updateNoteList(note.noteId)}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        )}
-        {/* <div>
-          {this.state.archiveNote.map(note => {
-            return <SideDrawer noteList={note} />;
-          })}
-        </div> */}
-      </div>
     );
-  }
+  }, [notelist]);
+
+  const updateNoteListPin = id => {
+    setNoteList(
+      notelist.map(nt => {
+        if (nt.noteId === id) {
+          NoteController.pinNote(id)
+            .then(response => {
+              console.log("Message :", response.data.message);
+            })
+            .catch(error => {
+              console.log("Message :", error.data.message);
+            });
+          console.log("bla bla");
+          nt.archiev = false;
+          nt.trash = false;
+          nt.pin = !nt.pin;
+        }
+        return nt;
+      })
+    );
+  };
+
+  const updateNoteListStatus = (id, status) => {
+    setNoteList(
+      notelist.map(note => {
+        if (note.noteId === id) {
+          if (status === "Archive") {
+            NoteController.archiveNote(note.noteId)
+              .then(response => {
+                console.log("Message :", response.data.message);
+              })
+              .catch(error => {
+                console.log("Message :", error.data.message);
+              });
+            note.archiev = true;
+            note.pin = false;
+            note.trash = false;
+          } else if (status === "Active") {
+            note.archiev = false;
+            note.trash = false;
+          } else if (status === "Trash") {
+            NoteController.deleteNote(this.props.noteId)
+              .then(response => {
+                console.log(response.data.message);
+              })
+              .catch(error => {
+                console.log(error.data.message);
+              });
+            note.archiev = false;
+            note.trash = true;
+          }
+        }
+        return note;
+      })
+    );
+  };
+  return (
+    <div>
+      {pinnedNote.map(n => {
+        return <div>{n.noteID}</div>;
+      })}
+      {props.status.active ? (
+        <div>
+          <strong style={{ paddingLeft: "18px" }}>PINNED</strong>
+          <div className="note-list row">
+            {pinnedNote.map(note => {
+              return (
+                <Note
+                  noteId={note.noteId}
+                  title={note.title}
+                  description={note.description}
+                  pin={note.pin}
+                  fnote={note}
+                  onPinClick={() => updateNoteListPin(note.noteId)}
+                  handleArchive={() =>
+                    updateNoteListStatus(note.noteId, "Archive")
+                  }
+                />
+              );
+            })}
+          </div>
+          <strong style={{ paddingLeft: "18px" }}>OTHERS</strong>
+          <div className="note-list row">
+            {unPinnedNote.map(note => {
+              return (
+                <Note
+                  noteId={note.noteId}
+                  title={note.title}
+                  description={note.description}
+                  pin={note.pin}
+                  fnote={note}
+                  onPinClick={() => updateNoteListPin(note.noteId)}
+                  handleArchive={() =>
+                    updateNoteListStatus(note.noteId, "Archive")
+                  }
+                />
+              );
+            })}
+          </div>
+        </div>
+      ) : props.status.archive ? (
+        <div>
+          <div className="note-list row">
+            {archiveNote.map(note => {
+              return (
+                <Note
+                  noteId={note.noteId}
+                  title={note.title}
+                  description={note.description}
+                  pin={note.pin}
+                  fnote={note}
+                  onPinClick={() => updateNoteListPin(note.noteId)}
+                  handleArchive={() =>
+                    updateNoteListStatus(note.noteId, "Active")
+                  }
+                />
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="note-list row">
+            {trashNote.map(note => {
+              return (
+                <Note
+                  noteId={note.noteId}
+                  title={note.title}
+                  description={note.description}
+                  pin={note.pin}
+                  fnote={note}
+                  handleTrash={() =>
+                    updateNoteListStatus(note.noteId, "Trash")
+                  }
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 export default NoteList;
