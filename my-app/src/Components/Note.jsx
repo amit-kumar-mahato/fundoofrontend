cc4b8dcc620dbc6690a41e4c9805ac43505451d2
amit-kumar-mahato <amitraj0589@gmail.com>
2020-02-07 19:36:05 +0530

    [Amit] refactored Note.jsx

--- my-app/src/Components/Note.jsx
+++ my-app/src/Components/Note.jsx
@@ -17,12 +17,11 @@ class Note extends Component {
       //iconShow: false,
       isPinned: false,
       show:false,
-      moreOption:false
+      note: props.fnote
     };
     this.handleClick = this.handleClick.bind(this);
     //this.handleCollaborator = this.handleCollaborator.bind(this);
     //this.handleIcon = this.handleIcon.bind(this);
-    this.handleMoreOption = this.handleMoreOption.bind(this);
   }
 
   handleClose = () => this.setState({show:false});
@@ -31,30 +30,37 @@ class Note extends Component {
   handleClick() {
     this.setState(oldState => ({ condition: !oldState.condition }));
   }
-  handleMoreOption() {
-    this.setState(prevState => ({moreOption: !prevState.moreOption}));
-  }
+
   // handleCollaborator() {
   //   this.setState(collaborator => ({ modelOpen: !collaborator.modelOpen }));
   // }
   // handleIcon() {
   //   this.setState(icons => ({ iconShow: !icons.iconShow }));
   // }
-  handlePinned = (noteid) =>{
-    NoteController.pinNote(this.props.noteId).then(response => {
-      console.log("Message :",response.data.message);
-    })
-    .catch(error => {
-      console.log("Message :",error.data.message);
-    })
-  }
-  handleArchive = (event) => {
-    NoteController.archiveNote(this.props.noteId).then(response => {
-      console.log("Message :",response.data.message);
-    })
-    .catch(error => {
-      console.log("Message :",error.data.message);
-    })
+  // handlePinned = (noteid) =>{
+  //   NoteController.pinNote(this.props.noteId).then(response => {
+  //     console.log("Message :",response.data.message);
+  //   })
+  //   .catch(error => {
+  //     console.log("Message :",error.data.message);
+  //   })
+  // }
+  // handleArchive = (event) => {
+  //   NoteController.archiveNote(this.props.noteId).then(response => {
+  //     console.log("Message :",response.data.message);
+  //   })
+  //   .catch(error => {
+  //     console.log("Message :",error.data.message);
+  //   })
+  // }
+  handleTrash = () => {
+    NoteController.deleteNote(this.props.noteId)
+      .then(response => {
+        console.log(response.data.message);
+      })
+      .catch(error => {
+        console.log(error.data.message);
+      });
   }
   render() {
     const { condition, modelOpen,moreOption} = this.state;
@@ -64,7 +70,7 @@ class Note extends Component {
           <div>
           <Card
             className="my-2 mr-2"
-            style={{ width: "15rem", borderRadius: "7px" }}
+            style={{ width: "15rem", borderRadius: "7px"}}
             //onClick={this.handleShow}
           >
             <Card.Body>
@@ -80,53 +86,13 @@ class Note extends Component {
               </div>
               <div onClick={this.handleShow}>{description}</div>
               <div style={{marginTop:'30px'}}>
-                <Icon handleClick={this.handleClick} handleMoreOption={this.handleMoreOption}/>
+                <Icon 
+                  handleClick={this.handleClick} 
+                  handleArchive={this.props.handleArchive}
+                  handleTrash={this.props.handleTrash}
+                  noteId={noteId}
+                  />
               </div>
-              {/* {iconShow ===true ? */}
-              {/* <div className="d-flex icon-div" style={{ cursor: "pointer" }}>
-                <div className="icon-div-content">
-                  <i
-                    className="fa fa-bell"
-                    title="Remind me"
-                    aria-hidden="true"
-                    onClick={this.handleClick}
-                  ></i>
-                </div>
-                <div className="icon-div-content">
-                  <i
-                    className="fa fa-user-plus"
-                    title="Collaborator"
-                    aria-hidden="true"
-                    onClick={this.handleCollaborator}
-                  ></i>
-                </div>
-                <div className="icon-div-content">
-                  <i
-                    className="fa fa-print"
-                    title="Change Color"
-                    aria-hidden="true"
-                  ></i>
-                </div>
-                <div className="icon-div-content">
-                  <i
-                    className="fa fa-file-image-o"
-                    title="Add image"
-                    aria-hidden="true"
-                  ></i>
-                </div>
-                <div className="icon-div-content">
-                  <i
-                    className="fa fa-archive"
-                    title="Archive"
-                    aria-hidden="true"
-                    onClick={this.handleArchive}
-                  ></i>
-                </div>
-                <div className="icon-div-content">
-                  <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-                </div>
-              </div> */}
-              {/* :<div></div>} */}
             </Card.Body>
             <div>
               {condition === true ? <Reminder noteId={noteId} /> : <div />}
