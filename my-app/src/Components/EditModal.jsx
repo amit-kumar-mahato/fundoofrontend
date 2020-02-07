2151d03cb801508d4fcd352236c9e721c8c66f2b
amit-kumar-mahato <amitraj0589@gmail.com>
2020-02-07 19:33:40 +0530

    [Amit] refactored EditModal.jsx

--- my-app/src/Components/EditModal.jsx
+++ my-app/src/Components/EditModal.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
-export default function EditModal() {
+import React from 'react';
+import {Modal} from 'react-bootstrap';
+export default function EditModal(props) {
+    console.log(props.show+"/ "+props.onHide);
     return (
-        <div>
-            
-        </div>
+            <Modal show={props.show} onHide={props.onHide} centered>
+                <div className="modal-main-div">
+                    <div className="modal-content">
+                        <div style={{display:'flex'}}>
+                            <input />
+                        </div>
+                        <div></div>
+                    </div>
+                </div>
+            </Modal>
     )
 }
