8507967eb2986f5948b4ba8ba4c7cf43512b191a
amit-kumar-mahato <amitraj0589@gmail.com>
2020-02-07 19:34:13 +0530

    [Amit] refactored Icon.jsx

--- my-app/src/Components/Icon.jsx
+++ my-app/src/Components/Icon.jsx
@@ -1,54 +1,96 @@
 import React, { useState } from "react";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, FormControl, Button } from "react-bootstrap";
 import NoteController from "../Controller/NoteController";
 
 export default function Icon(props) {
-  const [show, setShow] = useState(false);
-  const handleDelete = () => {
-    NoteController.deleteNote(props.noteId)
-      .then(response => {
-        console.log(response.data.message);
-      })
-      .catch(error => {
-        console.log(error.data.message);
-      });
-  };
-
+ 
+  const [date,setDate]= useState("");
+  const [time,setTime]= useState("");
+  const [dateError, setDateError] = useState("");
+  const [timeError, setTimeError] = useState("");
+  // const handleDelete = () => {
+  //   NoteController.deleteNote(props.noteId)
+  //     .then(response => {
+  //       console.log(response.data.message);
+  //     })
+  //     .catch(error => {
+  //       console.log(error.data.message);
+  //     });
+  // };
+  
+const handleDateChange = (e) =>{
+  setDate(e.target.value);
+}
+const handleTimeChange = (e) =>{
+  setTime(e.target.value);
+}
+const onSubmit = () => {
+  var reminder = {
+    datetime: date+" "+time,
+    noteId: props.noteId
+}
+setDate({date:""});
+setTime({time:""});
+NoteController.setReminder(reminder).then(response => {
+   console.log("RESPONSE :",response.data.message);
+})
+.catch(error =>{
+    console.log("ERROR"+error.data.message);
+})
+}
   return (
-    <div style={{ display: "flex", cursor: "pointer" }}>
-      <div className="">
+    <div className="icon-list">
+      {/* <div className="icon-div-content"> */}
         <Dropdown>
           <Dropdown.Toggle
-            id="dropdown-basic"
             bsPrefix="dropdown"
-            style={{ background: "none", border: "none", outline: "none" }}
+            style={{ background: "none", border: "none" }}
           >
             <i
               className="fa fa-bell"
               title="Remind me"
               aria-hidden="true"
-              //onClick={props.handleClick}
+              onClick={props.onClickReminderIcon}
               style={{ color: "black" }}
             ></i>
           </Dropdown.Toggle>
-          <Dropdown.Menu style={{lineHeight:'30px'}}>
-            <h5>Reminder:</h5>
-            <Dropdown.Item style={{display:'flex',cursor:'pointer'}}>
-              <div>Later today</div>
-              <div style={{marginLeft:'55px'}}>8:00PM</div>
-            </Dropdown.Item>
-            <Dropdown.Item style={{display:'flex',cursor:'pointer'}}>
-              <div>Tommorow</div>
-              <div style={{marginLeft:'55px'}}>8:00AM</div>
-            </Dropdown.Item>
-            <Dropdown.Item style={{display:'flex',cursor:'pointer'}}>
-              <div>Next Week</div>
-              <div style={{marginLeft:'55px'}}>MON,8:00AM</div>
-            </Dropdown.Item>
-            <Dropdown.Item>Pick date & time</Dropdown.Item>
+          <Dropdown.Menu>
+            <div className="reminder-div">
+              <div className="reminder-header">Reminder:</div>
+            <div style={{ marginTop: "17px" }}>
+              <input 
+                type="date" 
+                value={date}
+                onChange={handleDateChange}
+                required
+                className="reminder-input"
+              />
+            </div>
+            <div style={{ background: "#dadce0", height: "1px" }}></div>
+            {/* <small style={{ color: "red" }}>{this.state.dateErr}</small> */}
+            <div style={{ marginTop: "15px" }}>
+              <input
+                type="time"
+                value={time}
+                onChange={handleTimeChange}
+                required
+                className="reminder-input"
+              />
+            </div>
+            <div style={{ background: "#dadce0", height: "1px" }}></div>
+            </div>
+            {/* <small style={{ color: "red" }}>{this.state.timeErr}</small> */}
+
+            <button
+              className="float-right reminder-save-btn"
+              onClick={onSubmit}
+            >
+              Save
+            </button>
+           
           </Dropdown.Menu>
         </Dropdown>
-      </div>
+      {/* </div> */}
       <div className="icon-div-content">
         <i
           className="fa fa-user-plus"
@@ -80,7 +122,7 @@ export default function Icon(props) {
         <Dropdown.Toggle
           id="dropdown-more"
           bsPrefix="dropdown"
-          style={{background:'none',border:'none'}}
+          style={{ background: "none", border: "none" }}
         >
           <i
             className="fa fa-ellipsis-v"
@@ -90,7 +132,7 @@ export default function Icon(props) {
         </Dropdown.Toggle>
         <Dropdown.Menu>
           <Dropdown.Item>Add Label</Dropdown.Item>
-          <Dropdown.Item onClick={handleDelete}>Delete Label</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleTrash}>Delete Label</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
